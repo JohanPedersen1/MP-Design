da02ab89e90419102aaf4ae601d5a45c7060d2aa
AbdiAJ-17 <10665997@ucn.dk>
2025-04-08 11:32:02 +0200

    FriendContainer2

--- LP Management System/model/FriendContainer.java
+++ LP Management System/model/FriendContainer.java
@@ -26,9 +26,54 @@ public class FriendContainer
             for(Friend f : friends){
                 if (f.getName() == name)
                 {
-                
+                    return f; 
                 }
             }
         }
+        else package model;
+import java.util.ArrayList;
+
+
+
+public class FriendContainer
+{
+    private ArrayList<Friend> friends;
+    private static FriendContainer instance; 
+    
+    private FriendContainer()
+    {
+        
+        
+    }
+    
+    public static FriendContainer getInstance(){
+        if (instance == null){
+            instance = new FriendContainer();
+        }
+        return instance;
+    }
+    public Friend getFriend(String name, String phoneNo)
+    {
+        if (name != null && !name.isEmpty()){
+            for(Friend f : friends){
+                if (f.getName() == name)
+                {
+                    return f;
+                }
+            }
+        }
+        else if (phoneNo != null && !phoneNo.isEmpty()){
+            for(Friend f : friends){
+                if (f.getPhoneNo() == phoneNo)
+                {
+                    return f;
+                }
+            }
+        }
+        
+        return null;
+    }
+}
+
     }
 }
