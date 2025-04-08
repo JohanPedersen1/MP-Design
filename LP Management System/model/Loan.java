43e0f95c143b106eefbaf2a3b25d6c8b34a1a5bd
Fauerr <10665994@ucn.dk>
2025-04-08 11:06:16 +0200

    loan, copy, lp

--- LP Management System/model/Loan.java
+++ LP Management System/model/Loan.java
@@ -4,17 +4,21 @@ package model;
 
 public class Loan
 {
-    
+    // Instance varible describers for loan
     private int loanNumber;
     private String borrowDate;
     private String returnDate;
     private String status;
     
-    public Loan()
+    public Loan(int loanNumber, String borrowDate, String returnDate, String status)
     {
-        
+        this.loanNumber = loanNumber;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+        this.status = status;
     }
     
+    //get methods for Instance variables
     public int getLoanNumber(){
         return loanNumber;
     }
